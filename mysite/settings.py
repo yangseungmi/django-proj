26d36a02ec59fa50490baaa82642e84b105c8eff
Seungmi <0105627@gmail.com>
2022-02-08 01:50:40 +0900

    Add Allowed Hosts in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
+
